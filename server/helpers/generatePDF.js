5debb6453493d5668f83e7027e512904a24ce7ae
Anang Prayogo <anang1321@gmail.com>
2026-05-02 18:21:47 +0700

    feat: revisi

--- server/helpers/generatePDF.js
+++ server/helpers/generatePDF.js
@@ -309,7 +309,8 @@ const generatePDFBuffer = async (cv) => {
   });
 
   const page = await browser.newPage();
-  await page.setContent(html, { waitUntil: "networkidle0" });
+  page.setDefaultNavigationTimeout(15000);
+  await page.setContent(html, { waitUntil: "domcontentloaded" });
 
   const pdfBuffer = await page.pdf({
     format: "A4",
