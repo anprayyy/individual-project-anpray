a98638922f18e9cc370d59b5d5b18df13275bec8
Anang Prayogo <anang1321@gmail.com>
2026-06-01 20:52:00 +0700

    fix: change start script to node bin/www

--- server/routers/authRoutes.js
+++ server/routers/authRoutes.js
@@ -5,8 +5,6 @@ const AuthController = require("../controllers/AuthController");
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
 router.post("/google-verify", AuthController.googleVerify);
-router.get("/github", AuthController.githubLogin);
-router.get("/github/callback", AuthController.githubCallback);
 router.get("/google", AuthController.googleLogin);
 router.get("/google/callback", AuthController.googleCallback);
 
